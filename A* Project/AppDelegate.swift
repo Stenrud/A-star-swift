f1b10d4147ef962dd78a092ba29fffe824c6d9de
Stenrud <truls.stenrud@gmail.com>
2019-08-27 20:29:06 +0200

    first commit

---
+++ A* Project/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  A* Project
+//
+//  Created by Truls Stenrud on 27/08/2019.
+//  Copyright © 2019 Truls Stenrud. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ A* Project/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  A* Project
+//
+//  Created by Truls Stenrud on 27/08/2019.
+//  Copyright © 2019 Truls Stenrud. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
